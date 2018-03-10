3bafaaf60d9659692d8139be0a7c0c5281c48226
Sean McDemrott <seanjmac@hotmail.com>
2018-03-09 16:56:09 -0800

    added fourth spec passing

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,18 @@ $(document).ready(function() {
     var numberArray = [];
 
     for (index = 1; index <= userNumber; index += 1) {
-      if (index % 3 === 0) {
-        numberArray.push("ping");
+      if (index % 3 === 0 && index % 5 === 0) {
+        numberArray.push("pingpong");
       } else if (index % 5 === 0) {
         numberArray.push("pong");
+      } else if (index % 3 === 0) {
+        numberArray.push("ping");
       } else {
         numberArray.push(index);
       }
     }
     numberArray.map(function(list) {
-      $(".result").append("<li>" + list + "<li>")
+      $(".result").append('<li>' + list +'<li>');
     });
 
   });
