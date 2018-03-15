2602416bf0638bd6b5dbc88842c4b94c50b7b956
Sean McDemrott <seanjmac@hotmail.com>
2018-03-14 17:48:37 -0700

    Add seperation of BIZ and UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
+var counter = function(userNumber) {
+
+  var numberArray = [];
+  for (i = 1; i <= userNumber; i += 1) {
+    if (i % 15 === 0) {
+      numberArray.push("pingpong");
+    } else if (i % 5 === 0) {
+      numberArray.push("pong");
+    } else if (i % 3 === 0) {
+      numberArray.push("ping");
+    } else {
+      numberArray.push(i);
+    }
+  }
+  return numberArray;
+};
+
+
 $(document).ready(function() {
   $("form#number").submit(function(event) {
     event.preventDefault();
-    //debugger
-    var total = 0;
+    
+    $("#result").empty();
     var userNumber = parseInt($("input#userInput").val());
-    var numberArray = [];
+    var result = counter(userNumber);
     var li = $("<li>").appendTo("#result");
-    var json = { numbers: numberArray}
+    var json = { numbers: result}
     //var $resultContainer = $("#result");
 
-    for (index = 1; index <= userNumber; index += 1) {
-      if (index % 3 === 0 && index % 5 === 0) {
-        numberArray.push("pingpong");
-      } else if (index % 5 === 0) {
-        numberArray.push("pong");
-      } else if (index % 3 === 0) {
-        numberArray.push("ping");
-      } else {
-        numberArray.push(index);
-      }
-    }
+
 
 
     $(json.numbers).map(function(index, item) {
@@ -31,7 +39,7 @@ $(document).ready(function() {
     // });
     //
     // $("li", $resultContainer).slideDown(1500);
-    // numberArray.map(function(list) {
+    // result.map(function(list) {
     //   $("#result").append('<li>' + list +'<li>');
     // });
 
